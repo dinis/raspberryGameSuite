1b25e7d7e9c82af9f2425c81c6504b0ec3a165d0
Diogo Dinis <diogo@localhost.localdomain>
2017-08-09 13:21:37 +0100

    add md5 to password on client. I don't know if is better to use a special message to this exception

--- rGS-common/src/main/java/pt/dinis/common/core/Tools.java
+++ rGS-common/src/main/java/pt/dinis/common/core/Tools.java
@@ -5,7 +5,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Tools {
-    public static String stringToMd5(String string) throws NoSuchAlgorithmException {
+    public static String stringToMD5(String string) throws NoSuchAlgorithmException {
         MessageDigest md5 = MessageDigest.getInstance("MD5");
         md5.update(string.getBytes(), 0, string.length());
 
